88b36c4b899bb5bd3c94b82053324185e5632ed7
Andrew <andrew.m.holsaeter@gmail.com>
2024-05-11 01:59:17 +0200

    Hacky multiple modals

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,39 +1,73 @@
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
+function openModal1() {
+    document.getElementById("myModal1").style.display = "block";
 }
 
 // Close the Modal
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+function closeModal1() {
+    document.getElementById("myModal1").style.display = "none";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function plusSlides1(n) {
+    showSlides1(slideIndex1 += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentSlide1(n) {
+    showSlides1(slideIndex1 = n);
 }
 
-function showSlides(n) {
+function showSlides1(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("mySlides1");
     var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    var captionText = document.getElementById("caption1");
+    if (n > slides.length) { slideIndex1 = 1 }
+    if (n < 1) { slideIndex1 = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    slides[slideIndex1 - 1].style.display = "block";
+}
+
+
+
+
+function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+}
+
+// Close the Modal
+function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    console.log(n);
+    var i;
+    var slides = document.getElementsByClassName("mySlides2");
+    //var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption2");
+    if (n > slides.length) { slideIndex2 = 1 }
+    if (n < 1) { slideIndex2 = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+    slides[slideIndex2 - 1].style.display = "block";
 }

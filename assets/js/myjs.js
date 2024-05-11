6b847196ce29297ea2151ec914f138e99150e918
Andrew <andrew.m.holsaeter@gmail.com>
2024-05-11 07:36:47 +0200

    Single js for modal

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,72 +1,39 @@
-function openModal1() {
-    document.getElementById("myModal1").style.display = "block";
+var slideIndex = 1;
+//showSlides("drillbotics", slideIndex);
+
+function openModal(modal) {
+    var id = modal + "-modal"
+    slideIndex = 1;
+    document.getElementById(id).style.display = "block";
 }
 
 // Close the Modal
-function closeModal1() {
-    document.getElementById("myModal1").style.display = "none";
+function closeModal(modal) {
+    var id = modal + "-modal";
+    document.getElementById(id).style.display = "none";
+    slideIndex = 1;
 }
 
-var slideIndex1 = 1;
-showSlides1(slideIndex1);
-
 // Next/previous controls
-function plusSlides1(n) {
-    showSlides1(slideIndex1 += n);
+function plusSlides(modal, n) {
+    showSlides(modal, slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide1(n) {
-    showSlides1(slideIndex1 = n);
+function currentSlide(modal, n) {
+    showSlides(modal, slideIndex = n);
 }
 
-function showSlides1(n) {
+function showSlides(modal, n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides1");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption1");
-    if (n > slides.length) { slideIndex1 = 1 }
-    if (n < 1) { slideIndex1 = slides.length }
+    var id = modal + "-modal"
+    var capID = modal + "-caption"
+    var slides = document.getElementById(id).getElementsByClassName("mySlides");
+    var captionText = document.getElementById(capID);
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex1 - 1].style.display = "block";
-}
-
-
-
-
-function openModal2() {
-    document.getElementById("myModal2").style.display = "block";
-}
-
-// Close the Modal
-function closeModal2() {
-    document.getElementById("myModal2").style.display = "none";
-}
-
-var slideIndex2 = 1;
-showSlides2(slideIndex2);
-
-// Next/previous controls
-function plusSlides2(n) {
-    showSlides2(slideIndex2 += n);
-}
-
-// Thumbnail image controls
-function currentSlide2(n) {
-    showSlides2(slideIndex2 = n);
-}
-
-function showSlides2(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides2");
-    //var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption2");
-    if (n > slides.length) { slideIndex2 = 1 }
-    if (n < 1) { slideIndex2 = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex2 - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 }

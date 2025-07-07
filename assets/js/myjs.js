a007baaa8439dd23daf52bafb19ea406f7608013
Andrew <a.holsaeter@gmail.com>
2025-07-07 05:37:27 -0500

    Transition from user click

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -61,7 +61,6 @@ document.addEventListener('click', e => {
     const x = ((e.clientX - rect.left) / rect.width) * 100;
     const y = ((e.clientY - rect.top) / rect.height) * 100;
 
-    console.log(x,y);
 
     // Set transform-origin dynamically
     img.style.transformOrigin = `${x}% ${y}%`;
